e6d1f10aac694890e2f5adb3de194b5f4bcd69b0
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2024-09-02 15:29:44 +0200

    push will only replace the file if the device has one

--- push.sh
+++ push.sh
@@ -1,18 +1,15 @@
 git reset --hard
 git pull
 
-
 if [ ! -f ~/.bash_aliases ]; then
-    touch ~/.bash_aliases
+    rm configuration/.bash_aliases
+    cp ~/.bash_aliases configuration/.bash_aliases
 fi
-rm configuration/.bash_aliases
-cp ~/.bash_aliases configuration/.bash_aliases
 
 if [ ! -f ~/.bash_functions ]; then
-    touch ~/.bash_functions
+    rm configuration/.bash_functions
+    cp ~/.bash_functions configuration/.bash_functions
 fi
-rm configuration/.bash_functions
-cp ~/.bash_functions configuration/.bash_functions
 
 git add .
 git commit -m "Alias and Function update"
