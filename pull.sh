d297cc6b49c986b5d4958e323645e018298dc367
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2024-09-02 15:25:07 +0200

    pull will only remove file if it exists

--- pull.sh
+++ pull.sh
@@ -1,8 +1,12 @@
 git reset --hard && git pull
 
-rm ~/.bash_aliases
+if [ -f ~/.bash_aliases ]; then
+    rm ~/.bash_aliases
+fi
 cp ./configuration/.bash_aliases ~/.bash_aliases 
 
-rm ~/.bash_functions
+if [ -f ~/.bash_functions ]; then
+    rm ~/.bash_functions
+fi
 cp ./configuration/.bash_functions ~/.bash_functions
 
