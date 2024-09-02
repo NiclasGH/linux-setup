38114f496edbdb9cba2bf7b7bd4b1f8f2e15af63
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2024-09-02 14:04:31 +0200

    modify push file so it creates the file if its not already present

--- push.sh
+++ push.sh
@@ -1,9 +1,16 @@
 git reset --hard
 git pull
 
+
+if [ ! -f ~/.bash_aliases ]; then
+    touch ~/.bash_aliases
+fi
 rm configuration/.bash_aliases
 cp ~/.bash_aliases configuration/.bash_aliases
 
+if [ ! -f ~/.bash_functions ]; then
+    touch ~/.bash_functions
+fi
 rm configuration/.bash_functions
 cp ~/.bash_functions configuration/.bash_functions
 
