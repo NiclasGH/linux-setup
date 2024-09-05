0c01b2415840433d3c5394947990d96aaac8593d
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-09-05 23:13:22 +0200

    add xclip as dependecy for nvim on arch

--- scripts/arch/nvim_dependencies.sh
+++ scripts/arch/nvim_dependencies.sh
@@ -2,5 +2,5 @@
 
 # Idempotent
 
-sudo pacman -S --noconfirm --needed gcc repgrep python-virtualenv
+sudo pacman -S --noconfirm --needed gcc repgrep python-virtualenv xclip
 cargo install tree-sitter-cli
