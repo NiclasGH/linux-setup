4c905d85cc66711138531151cc42d9468101bcf4
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-09-13 08:06:17 +0200

    add autocrlf to git config

--- scripts/commons/git_config.sh
+++ scripts/commons/git_config.sh
@@ -3,3 +3,4 @@
 git config --global user.name "Niclas Kürschner"
 git config --global user.email "niclas.kuerschner@outlook.com"
 git config --global pull.rebase true
+git config --global core.autocrlf false
