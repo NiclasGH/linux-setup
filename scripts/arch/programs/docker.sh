0392f50d446072818c1023fddf5178e1582e9c3b
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-10-17 21:57:25 +0200

    split programs into many small files

---
+++ scripts/arch/programs/docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NON_ROOT_USER=$(id -un 1000)
+
+sudo pacman -S --noconfirm --needed docker docker-compose
+systemctl start docker.socket
+sudo gpasswd -a $NON_ROOT_USER docker # Allows user to run docker commands without root
+yay -S --noconfirm --needed docker-desktop
+
---
+++ scripts/commons/toolbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.2.32922.tar.gz
+tar -xzf toolbox.tar.gz
+TOOLBOX_DIR=$(find -name "*jetbrains*" -type d)
+$TOOLBOX_DIR/jetbrains-toolbox
+rm -rf $TOOLBOX_DIR
+rm -f toolbox.tar.gz
