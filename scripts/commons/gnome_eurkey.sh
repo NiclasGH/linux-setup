3ccb57f499c818f516438f1fd307470f695daeab
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-02-01 11:14:08 +0100

    fix: gnome needs to first enable hidden key layouts for eurkey

--- scripts/commons/gnome_eurkey.sh
+++ scripts/commons/gnome_eurkey.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [[ $DESKTOP_SESSION = "gnome" ]] then
+	gsettings set org.gnome.desktop.input-sources show-all-sources true
 	gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'eu'), ('xkb', 'de')]"
 fi
