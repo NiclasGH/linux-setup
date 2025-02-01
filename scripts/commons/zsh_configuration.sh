9657ebd3da88f0c3344d17bfd974ade31a3fa608
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-02-01 11:15:12 +0100

    fix: zsh config used > instead of >> which overwrote the zshrc which broke oh my zsh

--- scripts/commons/zsh_configuration.sh
+++ scripts/commons/zsh_configuration.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
-echo 'source ~/.bashrc' > ~/.zshrc
-
-tee -a ~/.zshrc << END
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
-END
-
-tee -a ~/.zshrc << END
-if [ -f ~/.bash_functions ]; then
-    . ~/.bash_functions
-fi
-END
-
-tee -a ~/.zshrc << END
-if [ -f ~/.bash_device ]; then
-    . ~/.bash_device
-fi
-END
-
+echo 'source ~/.bash_device' >> ~/.zshrc
+echo 'source ~/.bash_aliases' >> ~/.zshrc
+echo 'source ~/.bash_functions' >> ~/.zshrc
