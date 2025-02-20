b6e7a2ee7726d06087239b0e8f92aa522cbb8ba9
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2025-02-20 14:26:55 +0100

    adjust input message in init script

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ ssh-keygen -t ed25519
 echo ; echo "Add the following key to: https://github.com/settings/keys to be able to continue"
 cat ~/.ssh/id_ed25519.pub
 
-read -p "Press enter to continue"
+read -p "Press any key after adding the ssh key on github to continue"
 git clone git@github.com:NiclasGH/linux-setup.git ~/linux-setup
 
 # Initalize aliases
