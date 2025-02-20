c54f9e862923d83a7b84756c4d8d38281c71963f
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2025-02-20 11:02:06 +0100

    remove the common shell configurations

--- init.sh
+++ init.sh
@@ -11,9 +11,6 @@ then
     	exit 1
 fi
 
-
-DIRECTORY="linux-setup"
-
 git config --global user.name "Niclas Kürschner"
 git config --global user.email "niclas.kuerschner@outlook.com"
 
@@ -21,17 +18,14 @@ echo "Generating ssh key..."
 ssh-keygen -t ed25519
 
 echo ; echo "Add the following key to: https://github.com/settings/keys to be able to continue"
-cat /home/$USER/.ssh/id_ed25519.pub
+cat ~/.ssh/id_ed25519.pub
 
 read -p "Press enter to continue"
-git clone git@github.com:NiclasGH/linux-setup.git /home/$USER/$DIRECTORY
+git clone git@github.com:NiclasGH/linux-setup.git ~/linux-setup
 
 # Initalize aliases
-(cd /home/$USER/linux-setup; ./pull.sh)
-cp /home/$USER/$DIRECTORY/configuration/.bash_device /home/$USER/.bash_device
-echo "source /home/$USER/.bash_device" >> /home/$USER/.bashrc
-echo "source /home/$USER/.bash_functions" >> /home/$USER/.bashrc
-echo "source /home/$USER/.bash_aliases" >> /home/$USER/.bashrc
+git clone git@github.com:NiclasGH/shell-configuration.git ~/shell-configuration
+(cd ~/shell-configuration; ./init.sh)
 echo "Aliases successfully configured"
 
 echo ; echo "Initialization successful!"
