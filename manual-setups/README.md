73f144a03d373707cc2485d2e5e5575aa5c30d26
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-06-11 19:40:05 +0200

    add some manual install steps

---
+++ manual-setups/README.md
@@ -0,0 +1,7 @@
+* Dark Mode
+* Deactivate Hot Corner
+* Set Monitor Hz
+* Set Workspace Shortcuts
+* Start Steam through terminal and deactivate: 'Enable GPU accelerated rendering in web views'
+* Adjust Font to Inter and JetBrains Mono
+
--- scripts/config/font.sh
+++ scripts/config/font.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+# JetBrains Mono
 wget -O font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+unzip font.zip -d fonts
+sudo cp -f fonts/*.ttf /usr/share/fonts
+rm -r fonts
+rm font.zip
+
+# Inter
+wget -O font.zip https://github.com/rsms/inter/releases/download/v4.1/Inter-4.1.zip
 unzip font.zip -d fonts
 sudo cp -f fonts/*.ttf /usr/share/fonts
 rm -r fonts
