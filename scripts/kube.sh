ccfad52ff19b3b18f30d3bb2f22491178185f949
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-10-10 18:53:00 +0200

    feat: add k9s to install script

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo dnf install -y helm
+sudo dnf install -y helm k9s
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
