f39273d84bc5359340b16d1c70333f914a452fd7
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-12-12 17:44:20 +0100

    fix: add steam-devices for controller support

--- scripts/programs/gaming.sh
+++ scripts/programs/gaming.sh
@@ -3,4 +3,4 @@
 flatpak install -y flathub com.valvesoftware.Steam
 flatpak install -y flathub net.davidotek.pupgui2
 flatpak install -y flathub com.github.Matoking.protontricks
-sudo dnf install -y gamemode mangohud
+sudo dnf install -y gamemode mangohud steam-devices
